c7c2ed8cf07ae2dbdaf4719826b1e556fc7ee6b2
Fyodor <fekazar@gmail.com>
2024-11-17 10:07:13 +0300

    Compost

--- src/main/java/org/cyberchronicle/auth/controller/TokenController.java
+++ src/main/java/org/cyberchronicle/auth/controller/TokenController.java
@@ -15,6 +15,7 @@ import org.cyberchronicle.auth.service.UserService;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -41,7 +42,7 @@ public class TokenController {
         return tokenService.refresh(refreshAuthData.userId(), refreshAuthData.tokenId());
     }
 
-    @PostMapping("/auth")
+    @GetMapping("/auth")
     public void auth(HttpServletRequest request, HttpServletResponse response) {
         var authHeader = extractAuthHeader(request);
         try {
